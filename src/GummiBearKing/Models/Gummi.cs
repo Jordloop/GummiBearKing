22847424111a2d3e406a37443dc178ea8563233f
JordLoop <Jordloop@gmail.com>
2017-08-08 09:09:22 -0700

    Add Gummi Model

---
+++ src/GummiBearKing/Models/Gummi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GummiBearKing.Models
+{
+    public class Gummi
+    {
+        [Key]
+        public int GummiId { get; set; }
+        public string Name { get; set; }
+        public string Origin { get; set; }
+
+    }
+}
